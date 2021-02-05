1e46343f167a7e66575ae8e1ef0219b56ec01866
kev-kev <kevin.clark93@gmail.com>
2021-02-04 20:00:56 -0500

    dictionary api setup

--- frontend/src/Components/NewWordForm.js
+++ frontend/src/Components/NewWordForm.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export class NewWordForm extends Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   render() {
     return (
       <div class="container">
-        <form class="row align-items-center">
+        <form class="row align-items-center" onSubmit={this.handleSubmit}>
           <label class="sr-only" for="wordInput">
             Enter a Word
           </label>
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 const serviceAccount = require("./permissions.json");
+const fetch = require("node-fetch");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -12,6 +14,26 @@ const db = admin.firestore();
 
 const app = express();
 app.use(cors({ origin: true }));
+
+// Dictionary API setup
+const endpoint = "entries";
+const languageCode = "en-us";
+const wordID = "example";
+const url =
+  process.env.DICT_APP_URL +
+  endpoint +
+  "/" +
+  languageCode +
+  "/" +
+  wordID.toLowerCase();
+const headers = {
+  app_id: process.env.DICT_APP_ID,
+  app_key: process.env.DICT_APP_KEY,
+};
+
+getDefinitionsFromJson = (json) => {
+  return json.results[0].lexicalEntries[0].entries[0].senses[0].definitions;
+};
 
 app.get("/api/index", (req, res) => {
   (async () => {
@@ -47,16 +69,23 @@ app.get("/api/:id", (req, res) => {
 });
 
 app.post("/api/create", (req, res) => {
-  (async () => {
-    const entry = req.body;
-    try {
-      await db.collection("entries").add(entry);
-      return res.status(200).send(entry);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
+  fetch(url, {
+    headers: headers,
+  })
+    .then((r) => r.json())
+    .then((json) => {
+      (async () => {
+        const entry = req.body;
+        entry.dictionaryDefinitions = getDefinitionsFromJson(json);
+        try {
+          await db.collection("entries").add(entry);
+          return res.status(200).send(entry);
+        } catch (error) {
+          console.log(error);
+          return res.status(500).send(error);
+        }
+      })();
+    });
 });
 
 app.put("/api/:id", (req, res) => {
